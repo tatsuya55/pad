859886765d7252d9ff50b509295a63c8134ee2d5
小先生 <1761970318@qq.com>
2022-09-09 14:10:16 +0800

    详情页

--- pad-core/src/main/java/com/pad/entity/CompanyMaterial.java
+++ pad-core/src/main/java/com/pad/entity/CompanyMaterial.java
@@ -59,8 +59,11 @@ public class CompanyMaterial implements Serializable {
     @ApiModelProperty(value = "纳税记录")
     private String records;
 
-    @ApiModelProperty(value = "状态 0未审核，1审核通过，-1审核失败")
+    @ApiModelProperty(value = "状态 审核通过，审核不通过")
     private Integer status;
+
+    @ApiModelProperty(value = "抵押物证明")
+    private String collateralPhoto;
 
 
     private CompanyInfo companyInfo;
