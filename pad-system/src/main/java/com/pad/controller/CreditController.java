498795e3f8f32189695e6708bf66334aead67486
Tatsuya <2506682863@qq.com>
2022-10-05 09:53:27 +0800

    计算每月分期，原定还款日期

--- pad-system/src/main/java/com/pad/controller/CreditController.java
+++ pad-system/src/main/java/com/pad/controller/CreditController.java
@@ -3,12 +3,11 @@ package com.pad.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.pad.entity.Credit;
-import com.pad.entity.LoanInfo;
-import com.pad.entity.Profit;
+import com.pad.entity.*;
 import com.pad.response.R;
-import com.pad.service.CreditService;
-import com.pad.service.ProfitService;
+import com.pad.service.*;
+import com.pad.utils.LoanCalculator;
+import com.pad.vo.LoanData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -17,6 +16,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -37,6 +39,16 @@ public class CreditController {
 
     @Autowired
     private ProfitService profitService;
+
+    @Autowired
+    private LoanInfoService loanInfoService;
+
+    @Autowired
+    private BankService bankService;
+
+    @Autowired
+    private PeriodizationService periodizationService;
+
 
     @PreAuthorize("@me.hasAuthority('credit:info:list')")
     @ApiOperation("放款信息表分页显示")
@@ -66,8 +78,53 @@ public class CreditController {
             @ApiParam(name = "credit",value = "放款",required = true)
             @RequestBody Credit credit
     ){
-        System.out.println(credit);
+        //放款
         creditService.save(credit);
+        //放款的同时 计算分期还款时间
+        List<Periodization> periodizationList = new ArrayList<>();
+        String loanId = credit.getLId();
+        LoanInfo loanInfo = loanInfoService.getById(loanId);
+        Bank bank = bankService.getById(loanInfo.getBankNo());
+        LoanData loanData = new LoanData();
+        //贷款金额
+        loanData.setPrincipal(loanInfo.getAmount());
+        //年利率
+        loanData.setRate(bank.getBorrowYearRate());
+        //贷款总期数
+        Integer term = loanInfo.getPeriod();
+        loanData.setTerm(term);
+        //还款方式
+        Integer type = loanInfo.getReturnMethod();
+        //计算数据
+        LoanData data = null;
+        if (1==type){
+            //等额本息还款
+            data = LoanCalculator.EqualPrincipalandInterestMethod(loanData);
+        }
+        if (2==type){
+            //等额本金还款
+            data = LoanCalculator.EqualPrincipalMethod(loanData);
+        }
+        //将计算的数据封装到分期还款表
+        double[][] detail = data.getDetail();
+        //原定还款时间根据放款时间 每月加一
+        Calendar ct=Calendar.getInstance();
+        Date createTime = credit.getCreateTime();
+        ct.setTime(createTime);
+        for (int i = 0; i < term; i++) {
+            Periodization periodization = new Periodization();
+            periodization.setLId(loanId);
+            periodization.setCapital(detail[i][0]);
+            periodization.setInterest(detail[i][1]);
+            periodization.setCi(detail[i][2]);
+            periodization.setPeriods(i+1);
+            // 原定还款时间根据放款时间 每月加一
+            ct.add(Calendar.MONTH, 1);
+            periodization.setOriginallyTime(ct.getTime());
+            periodizationList.add(periodization);
+        }
+        //保存到数据库
+        periodizationService.saveBatch(periodizationList);
         //放款的同时 添加平台收益信息
         Profit profit = new Profit();
         profit.setCId(credit.getId());
